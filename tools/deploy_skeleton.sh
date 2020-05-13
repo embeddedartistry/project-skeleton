28845eab8446b453b512a309d9c455c57eccac42
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-13 13:15:19 -0700

    Update deploy script: copy files, setup git

--- tools/deploy_skeleton.sh
+++ tools/deploy_skeleton.sh
@@ -23,7 +23,7 @@ while getopts "rhs" opt; do
 	h) # Help
 		echo "Usage: deploy_skeleton.sh [optional ags] dest_dir"
 		echo "Optional args:"
-		echo "	-r: Assume non-git environment and install submodule files directly."
+		echo "	-r: Assume non-git environment. Installs submodule files directly."
 		echo "	-s: Don't use submodules, and copy files directly"
 		exit 0
 	;;
@@ -64,19 +64,45 @@ fi
 find . -name ".DS_Store" -exec rm {} \;
 
 CORE_FILES="docs src test tools .clang-format .clang-tidy Makefile meson.build meson_options.txt README.md"
-SUBMODULES="build"
 GIT_FILES=".gitattributes .github .gitignore"
-if [ $USE_SUBMODULES == 1 ]; then
-	GIT_FILES+=" .gitmodules"
-fi
 
-# Copy core skeleton files to the destination
+SUBMODULE_DIRS=("build")
+SUBMODULE_URLS=("git@github.com:embeddedartistry/meson-buildsystem.git")
+
+# Copy skeleton files to the destination
 cp -r $CORE_FILES $DEST_DIR
 mkdir -p $DEST_DIR/subprojects
 cp subprojects/*.wrap $DEST_DIR/subprojects
-cp -r $GIT_FILES $DEST_DIR
-
+rm $DEST_DIR/tools/deploy_skeleton.sh
+if [ $USE_GIT == 1 ]; then
+	cp -r $GIT_FILES $DEST_DIR
+fi
 if [ $USE_SUBMODULES == 0 ]; then
-	git submodule update --init
-	cp -r $SUBMODULES $DEST_DIR
+	git submodule update --init --recursive
+	cp -r ${SUBMODULE_DIRS[@]} $DEST_DIR
+fi
+
+## The following operations all take place in the destination directory
+cd $DEST_DIR
+
+# Commit Files
+if [ $USE_GIT == 1 ]; then
+	git add --all
+	git commit -m "Initial commit of project skeleton files."
+fi
+
+# Initialize Submodules
+if [ $USE_SUBMODULES == 1 ]; then
+	for index in ${!SUBMODULE_URLS[@]}; do
+		git submodule add ${SUBMODULE_URLS[$index]} ${SUBMODULE_DIRS[$index]}
+	done
+	git commit -m "Add submodules from project skeleton."
+else
+	# Remove any residual git files from submodule directories
+	find ${SUBMODULE_DIRS[@]} -name ".git*" -exec rm -rf {} \;
+fi
+
+# Push all changes to the server
+if [ $USE_GIT == 1 ]; then
+	git push || echo "WARNING: git push failed: check repository."
 fi
