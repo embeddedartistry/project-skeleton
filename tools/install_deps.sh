b0839dd319826ee3d0b30c155eb3663dcd81747f
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-14 09:03:10 -0700

    Remove vale from apt packages on Linux

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -11,7 +11,7 @@ BREW_PACKAGES=("python3" "ninja" "wget" "gcc@7" "gcc@8" "gcc@9" "llvm" "adr-tool
 BREW_PACKAGES+=("vale" "doxygen" "cppcheck" "clang-format" "gcovr" "lcov" "sloccount")
 # TODO: test app - do I need to add anything for versions?
 APT_PACKAGES=("python3" "python3-pip" "ninja-build" "wget" "build-essential" "clang" "lld" "llvm")
-APT_PACKAGES+=("clang-tools" "libcmocka0" "libcmocka-dev" "pkg-config" "vale" "sloccount")
+APT_PACKAGES+=("clang-tools" "libcmocka0" "libcmocka-dev" "pkg-config" "sloccount")
 APT_PACKAGES+=("doxygen" "cppcheck" "gcovr" "lcov" "clang-format" "clang-tidy" "clang-tools")
 APT_PACKAGES+=("gcc-7" "g++-7" "gcc-8" "g++-8" "gcc-9" "g++-9")
 PIP3_PACKAGES=("meson" "lizard")
