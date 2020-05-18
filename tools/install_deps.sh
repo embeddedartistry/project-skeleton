ab06bdf3970a9c3cb00b3191ddb18426c441c6cc
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-18 10:10:41 -0700

    Convert options to getopts, add option to update environment files

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# This script takes one optional argument. If "update" is supplied, dependencies will be
-# updated instead of installed.
+# This script takes two optional arguments.
+# -u will run an update process instead of install process
+# -e will modify enviroment files during the install process
 #
 # You can override the installation path for toolchains by defining TOOLCHAIN_INSTALL_DIR
 # You can override the installation path for tools by defining TOOL_INSTALL_DIR
@@ -17,6 +18,7 @@ TOOL_DISABLE_SUDO=${TOOL_DISABLE_SUDO:-0}
 
 # Commands
 UPDATE=0
+UPDATE_ENV=0
 PIP_UPDATE=
 BREW_COMMAND="install"
 APT_COMMAND="install"
@@ -35,12 +37,29 @@ APT_PACKAGES+=("doxygen" "cppcheck" "gcovr" "lcov" "clang-format" "clang-tidy" "
 APT_PACKAGES+=("gcc-7" "g++-7" "gcc-8" "g++-8" "gcc-9" "g++-9")
 PIP3_PACKAGES=("meson" "lizard" "thefuck")
 
-if [ "$1" == "update" ]; then
-	UPDATE=1
-	PIP_UPDATE="--upgrade"
-	BREW_COMMAND="upgrade"
-	APT_COMMAND="upgrade"
-fi
+while getopts "euh" opt; do
+  case $opt in
+  	e) UPDATE_ENV=1
+	;;
+	u)	UPDATE=1
+		PIP_UPDATE="--upgrade"
+		BREW_COMMAND="upgrade"
+		APT_COMMAND="upgrade"
+	;;
+	h) # Help
+		echo "Usage: install_deps.sh [optional ags]"
+		echo "Optional args:"
+		echo "	-u: Run an update instead of install"
+		echo "  -e: Include environment setup during install process (.bashrc + .bash_profile)"
+		exit 0
+	;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# Shift off the getopts args, leaving us with positional args
+shift $((OPTIND -1))
 
 if [ "$(uname)" == "Darwin" ]; then
 	if [ $UPDATE == 1 ]; then
@@ -100,6 +119,20 @@ else
 	${TOOLS_SUDO} mkdir -p ${TOOL_INSTALL_DIR}
 	cd ${TOOL_INSTALL_DIR}
 	${TOOLS_SUDO} git clone https://github.com/npryce/pottery.git --recursive
+fi
+
+#############################
+# Install Environment Files #
+#############################
+
+if [ $UPDATE == 0 ]; then
+	if [ $UPDATE_ENV == 1 ]; then
+		# Assume that the two scripts are contained in the same directory
+		cd $STARTING_DIR
+		source $(dirname $0)/setup_env.sh
+	else
+		echo "You may need to manually modify your PATH to reference the programs in $TOOLCHAIN_INSTALL_DIR and $TOOL_INSTALL_DIR"
+	fi
 fi
 
 # End in the starting directory
